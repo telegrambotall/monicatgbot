274e99b6415f11b814fa533bb443fc96c8684488
NB <45740739+HyperNB@users.noreply.github.com>
2019-05-12 02:22:58 +0530

    Fixed gban aka antispam😅

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -94,7 +94,7 @@ else:
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
-    STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
+    STRICT_ANTISPAM = bool(os.environ.get('STRICT_GBAN', False))
     WORKERS = int(os.environ.get('WORKERS', 8))
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
